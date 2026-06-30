62dfd8395a9851d1586b4d3e1589128dae3e0ac6
Rudraksh Bhardwaj <rudraksh.25b01010396@abes.ac.in>
2026-06-30 17:21:51 +0530

    dsa

---
+++ Score_of_a_String.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+class Solution {
+public:
+    int scoreOfString(string s) {
+        
+        int sum=0;
+        for (int i = 0; i < s.length() - 1; i++) {
+        sum = sum+(abs(s[i] - s[i+1]) );
+    }
+    return sum;
+    }
+};
