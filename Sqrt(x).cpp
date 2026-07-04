ac3b8cc280178f9ebc29cf6e63d2f5b2d23645d8
Rudraksh Bhardwaj <rudraksh.25b01010396@abes.ac.in>
2026-07-04 22:03:33 +0530

    dsa

---
+++ Sqrt(x).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0 || x == 1)
+            return x;
+
+        int left = 1, right = x;
+        int ans = 0;
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+
+            if (mid * mid == x)
+                return mid;
+            else if (mid * mid < x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
